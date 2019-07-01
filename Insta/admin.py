47e6545bb3b019c4d368284a4b0aee5c0303018f
WenTailai <wentailai.jiuzhang@gmail.com>
2019-07-01 13:09:54 -0400

    Add post model

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from Insta.models import Post
+
 # Register your models here.
+
+admin.site.register(Post)
--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality': 100},
+        blank=True,
+        null=True,
+        )
+
+    def __str__(self):
+        return self.title
